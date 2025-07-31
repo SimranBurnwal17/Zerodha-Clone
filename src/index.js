76b7f7971ece0637a2538caa67f615c88e66cf14
SimranBurnwal17 <simranburnwal888@gmail.com>
2025-07-31 13:18:20 +0530

    Add project files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import HomePage from './landing_page/home/HomePage';
+import PricingPage from './landing_page/pricing/PricingPage';
+import SupportPage from './landing_page/support/SupportPage'; 
+import AboutPage from './landing_page/about/AboutPage';
+import Footer from './landing_page/Footer';
+import Navbar from './landing_page/Navbar';
+import ProductsPage from './landing_page/products/ProductsPage';
+import NotFound from './landing_page/NotFound';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+  <Navbar/>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/pricing" element={<PricingPage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/support" element={<SupportPage />} />
+      <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer/>
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/landing_page/support/CreateTicket.js
@@ -0,0 +1,52 @@
+import React from 'react';
+function CreateTicket() {
+    return (
+        <div className="container">
+      <div className="row p-5 mt-5 mb-5">
+        <h1 className="fs-2">
+          To create a ticket, select a relevant topic
+        </h1>
+      
+      <div className="col-4 p-5 mt-5 mb-5">
+        <h1 className="fs-4 mb-4"><i class="fa fa-plus-circle" aria-hidden="true"></i>
+          Account Opening
+        </h1>
+        <a href='' style={{ textDecoration: 'none', lineHeight:"2.5" }}>Online Account Opening</a> <br/>
+        <a href='' style={{ textDecoration: 'none', lineHeight:"2.5"}}>Offline Account Opening</a> <br/>
+        <a href='' style={{ textDecoration: 'none', lineHeight:"2.5" }}>Company, Partnership and HUF Account Opening</a> <br/>
+        <a href='' style={{ textDecoration: 'none', lineHeight:"2.5"}}>NRI Account Opening</a> <br/>
+        <a href='' style={{ textDecoration: 'none', lineHeight:"2.5" }}>Charges at Zerodha</a> <br/>
+        <a href='' style={{ textDecoration: 'none', lineHeight:"2.5" }}>Zerodha IDFC FIRST Bank 3-in-1 Account</a> <br/>
+        <a href='' style={{ textDecoration: 'none', lineHeight:"2.5" }}>Getting Started</a> <br/>
+        </div>
+        <div className="col-4 p-5 mt-5 mb-5">
+        <h1 className="fs-4 mb-4"><i class="fa fa-user-circle" aria-hidden="true"></i>
+          Your Zerodha Account
+        </h1>
+        <a href='' style={{ textDecoration: 'none', lineHeight:"2.5" }}>Login Credentials</a> <br/>
+        <a href='' style={{ textDecoration: 'none', lineHeight:"2.5"}}>Account Modification and Segment Addition</a> <br/>
+        <a href='' style={{ textDecoration: 'none', lineHeight:"2.5" }}>DP ID and bank details</a> <br/>
+        <a href='' style={{ textDecoration: 'none', lineHeight:"2.5"}}>Your Profile</a> <br/>
+        <a href='' style={{ textDecoration: 'none', lineHeight:"2.5" }}>Transfer and conversion of sales</a> <br/>
+        
+        </div>
+        <div className="col-4 p-5 mt-5 mb-5">
+        <h1 className="fs-4 mb-4"><i class="fa fa-bar-chart" aria-hidden="true"></i>
+          Your Zerodha Account
+        </h1>
+        <a href='' style={{ textDecoration: 'none', lineHeight:"2.5" }}>Margin/leverage, Product and Order types</a> <br/>
+        <a href='' style={{ textDecoration: 'none', lineHeight:"2.5"}}>Kite Web and Mobile</a> <br/>
+        <a href='' style={{ textDecoration: 'none', lineHeight:"2.5" }}>Trading FAQs</a> <br/>
+        <a href='' style={{ textDecoration: 'none', lineHeight:"2.5"}}>Corporate Actions</a> <br/>
+        <a href='' style={{ textDecoration: 'none', lineHeight:"2.5" }}>Sentinel</a> <br/>
+        <a href='' style={{ textDecoration: 'none', lineHeight:"2.5" }}>Kite API</a> <br/>
+        <a href='' style={{ textDecoration: 'none', lineHeight:"2.5" }}>Pi and other platforms</a> <br/>
+        <a href='' style={{ textDecoration: 'none', lineHeight:"2.5" }}>Stockerports+</a> <br/>
+        <a href='' style={{ textDecoration: 'none', lineHeight:"2.5" }}>GTT</a> <br/>
+        </div> 
+      </div>
+      </div> 
+    );
+}
+
+export default CreateTicket;
